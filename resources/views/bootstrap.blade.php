b5bc46c6d2d222a61f0e7b6e3f28d695dae38b7e
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-07-14 19:23:38 +0700

    :sparkle: Showing server address and HTTP host

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -29,7 +29,16 @@
 
     <div class="container">
         @yield('content')
+        <hr/>
+
+        <footer>
+            <p>
+                Server address: <code>{{ $_SERVER['REMOTE_ADDR'] }}</code><br/>
+                HTTP host: <code>{{ $_SERVER['HTTP_HOST'] }}</code>
+            </p>
+        </footer>
     </div>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
