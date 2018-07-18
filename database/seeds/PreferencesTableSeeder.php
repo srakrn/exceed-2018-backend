dccfb0efbc83e60460e68e9331b2831e7c9425f7
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-07-18 20:02:18 +0700

    :sparkle: Preferences seeder

---
+++ database/seeds/PreferencesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PreferencesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('preferences')->insert([
+            'id' => 'sanitize_results',
+            'value' => 'true',
+        ]);
+        DB::table('preferences')->insert([
+            'id' => 'max_history_limit',
+            'value' => 100,
+        ]);
+    }
+}
