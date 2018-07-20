b8a5b29512c7c18597a4c82d5ea26e368ee4764c
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-07-20 18:37:40 +0700

    :sparkle: Menu to stats page

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -24,6 +24,9 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/stats">Statistics</a>
+                </li>
                 @guest
                 @else
                     <li class="nav-item">
